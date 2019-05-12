062a843e31c02fc63f1f9a0c379189ca0c0e97c5
陈世亮 <352056038@qq.com>
2019-05-12 21:09:52 +0800

    express配置跨域

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 var createError = require('http-errors');
 var express = require('express');
+var app = express();
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+app.all("*", function (req, res, next) {
+  //设置允许跨域的域名，*代表允许任意域名跨域
+  res.header("Access-Control-Allow-Origin", "*");
+  //允许的header类型
+  res.header("Access-Control-Allow-Headers", "content-type");
+  //跨域允许的请求方式 
+  res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
+  if (req.method.toLowerCase() == 'options')
+    res.send(200);  //让options尝试请求快速结束
+  else
+    next();
+})
 
 // 引入路由
+var noticesRouter = require('./routes/notices');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testpostRouter = require('./routes/testpost.js')
 var getlistRouter = require('./routes/getlist.js')
 
-var app = express();
-var noticesRouter = require('./routes/notices');
+
 app.use('/notices', noticesRouter);
 
 // view engine setup
@@ -36,6 +49,9 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+
+
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -46,6 +62,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 
 
